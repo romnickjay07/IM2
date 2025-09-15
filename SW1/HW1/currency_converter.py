c3a1eb036b705c304be3c6728776983edada8cff
Your Name <23ur0635@psu.edu.ph>
2025-09-15 19:37:55 +0800

    newcode currency

--- SW1/HW1/currency_converter.py
+++ SW1/HW1/currency_converter.py
@@ -1,22 +1,27 @@
 def convert_currency(dollar):
-    rate_rupee = 83.12 
-    rate_pound = 0.76       
-    rate_yuan = 7.31        
-
+    rate_rupee = 83.12   
+    rate_pound = 0.76    
+    rate_yuan = 7.31     
     rupee = dollar * rate_rupee
     pound = dollar * rate_pound
     yuan = dollar * rate_yuan
-    return rupee, pound, yuan 
+    return rupee, pound, yuan
+
+dollars = []
 
 while True:
     value = input("Enter dollar ($) (* to exit): ")
     if value == "*":
-        print("Bye")
+        print("\nBye\n")
         break
     try:
         dollar = float(value)
-        rupee, pound, yuan = convert_currency(dollar)
-        print(f"\n{'Dollar ($)':<15}{'Indian Rupee (₹)':<20}{'British Pound (£)':<20}{'Chinese Yuan (¥)':<20}")
-        print(f"{dollar:<15.2f}{rupee:<20.2f}{pound:<20.2f}{yuan:<20.2f}\n")
+        dollars.append(dollar)
     except ValueError:
         print("Invalid input, please enter a number or * to exit.\n")
+
+if dollars:
+    print(f"\n{'Dollar ($)':<15}{'Indian Rupee (₹)':<20}{'British Pound (£)':<20}{'Chinese Yuan (¥)':<20}")
+    for d in dollars:
+        rupee, pound, yuan = convert_currency(d)
+        print(f"{d:<15.2f}{rupee:<20.2f}{pound:<20.2f}{yuan:<20.2f}")
